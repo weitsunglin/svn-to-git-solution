fe35e188419ccc2e2521a3713b87cd35d348b27d
OLD_RD2林瑋琮 <wilson.lwt.tw@gmail.com>
2024-05-07 14:00:09 +0800

    Update git-svn.sh

--- git-svn.sh
+++ git-svn.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-# SVN 倉庫的 URL
-SVN_REPO_URL="http://192.168.1.183/svn/ManganDahen/branches/Release"
+# 设置游戏名称
+gameName="ManganDahen"
 
-# GitHub 倉庫的 URL
-GITHUB_REPO_URL="https://github.com/OLD-RD2/TMDC_ManganDahen"
+# SVN 仓库的 URL
+SVN_REPO_URL="http://192.168.1.183/svn/${gameName}/branches/Release"
 
-# 克隆 SVN 數據，限制為最新的 20 筆 commit
-mkdir svn-to-git
-cd svn-to-git
-git svn clone $SVN_REPO_URL . -rHEAD~19:HEAD --authors-file=authors.txt
+# GitHub 仓库的 URL
+GITHUB_REPO_URL="https://github.com/OLD-RD2/TMDC_${gameName}"
 
-# SVN 克隆完成後，預設在 master 分支
-# 我們需要將它重命名為 Release，以匹配 GitHub 上的分支名稱
+# 克隆 SVN 数据，限制为最新的 20 篇 commit
+mkdir "${gameName}"
+cd "${gameName}"
+git svn clone "${SVN_REPO_URL}" . -rHEAD~19:HEAD --authors-file=authors.txt
+
+# SVN 克隆完成后，默认在 master 分支
+# 我们需要将它重命名为 Release，以匹配 GitHub 上的分支名称
 git branch -m master Release
 
-# 設定遠程倉庫
-git remote add origin $GITHUB_REPO_URL
+# 设置远程仓库
+git remote add origin "${GITHUB_REPO_URL}"
 
 # 推送到 GitHub 的 Release 分支
 git push -u origin Release
